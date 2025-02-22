4095c6a9333a5bb9dbd0922541d44f2374e1f256
RafaelSeri <142455444+RafaelSeri@users.noreply.github.com>
2025-02-22 02:37:18 -0300

    Update datos_limpios.py

--- datos_limpios.py
+++ datos_limpios.py
@@ -1,9 +1,21 @@
-import pandas as pd
-import duckdb as dd
-#%% cargo los datasets
-centros=pd.read_csv('centros_culturales.csv')
-padron_ee=pd.read_excel('2022_padron_oficial_establecimientos_educativos.xlsx',skiprows=6)
-censo=pd.read_excel('padron_poblacion.xlsX')
+#%% Introducción
+
+"""
+GRUPO: ÑOQUIS DE LA ABUELA
+
+INTEGRANTES:
+    SERI RAFAEL (362/23)
+    ANDINA SILVA AUGUSTO (1344/23)
+    ARTUFFO TOMÁS (721/23)
+    
+EN ESTE CÓDGIO ENCONTRARÁN LAS SECCIONES DE PROCESAMIENTO DE DATOS
+Y ANÁLISIS DE DATOS.
+EN PROCESAMIENTO DE DATOS ESTAREMOS CREANDO Y LIMPIANDO LAS TABLAS DEL
+MODELO RELACIONAL EN 3FN.
+EN ANÁLISIS DE DATOS ESTAREMOS CREANDO LAS CONSULTAS SQL QUE NOS PIDEN
+Y LOS MATERIALES DE VISUALIZACIÓN.
+
+"""
 
 #%% PROCESAMIENTO DE DATOS
 
@@ -13,13 +25,14 @@ ESTAS TABLAS SON:
     departamento, poblacion, centro_cult, mail_centros, usa_mail y est_edu
 """
 
-#%% Creacion de la tabla provincia
+#%% cargo los datasets
 
-#junto las provincias con sus respectivos id_prov
-provincia=dd.sql("""
-SELECT DISTINCT ID_PROV, Provincia AS prov
-FROM centros                
-                """).df()
+import pandas as pd
+import duckdb as dd
+
+centros=pd.read_csv('centros_culturales.csv')
+padron_ee=pd.read_excel('2022_padron_oficial_establecimientos_educativos.xlsx',skiprows=6)
+censo=pd.read_excel('padron_poblacion.xlsX')
 
 #%% Creacion de la tabla departamento
 
@@ -117,12 +130,20 @@ SELECT FLOOR(id_depto/1000) AS id_prov, depto, (CASE WHEN id_depto=94015 THEN 94
 FROM pobl_grupos
                     """).df()                
 
+#junto las provincias con sus respectivos id_prov
+id_prov=dd.sql("""
+SELECT DISTINCT ID_PROV, (CASE WHEN Provincia LIKE '%Ciudad%' THEN 'CABA'
+                          WHEN Provincia LIKE '%Tierra%'THEN 'Tierra del Fuego'
+                          ELSE Provincia END) AS prov
+FROM centros                
+                """).df()
+
 #creo la tabla departamento con el nombre de la provincia en vez del id_prov
 departamento=dd.sql("""
 SELECT p.prov, d.depto, d.id_depto, d.pobl_jardin, d.pobl_primaria,
         d.pobl_secundaria
 FROM departamento_prov AS d
-INNER JOIN provincia AS p
+INNER JOIN id_prov AS p
 ON p.ID_PROV=d.id_prov                     
                     """).df()
 
@@ -134,20 +155,32 @@ FROM departamento_prov
                  """).df()
 
 
+#%% Creacion de la tabla provincia
+
+provincia=dd.sql("""
+SELECT prov
+FROM id_prov                 
+                 """).df()
+
 #%% Creacion de la tabla centro_cult
 
+#me quedo con las columnas que me interesan
 cc_uno=centros[['ID_DEPTO','Nombre', 'Mail ','Capacidad']]
 
-cc_uno['ID_cc']=0 #ID_cc será la clave de la tabla
+#ID_cc será la clave de la tabla
+cc_uno['ID_cc']=0 
 
 #asignamos un valor a ID_cc
 for i,e in cc_uno.iterrows():
         cc_uno.loc[i,'ID_cc']=i
 
+#cambio el nombre de la columna para que sea más cómodo
 cc_uno=cc_uno.rename(columns={'Mail ':'Mail'})
-            
+ 
+#me quedo con las columnas propias del modelo relacional           
 centro_cult=cc_uno[['ID_cc','ID_DEPTO','Capacidad']]
 
+#reemplzaco los NaN en 'Capacidad' por 0
 centro_cult=centro_cult.fillna(value=0)
 
 #%% creacion de la tabla mail_centros
@@ -186,11 +219,14 @@ FROM usa_mail_aux
 WHERE Mail LIKE '%@%'                   
                     """).df()
 
+#solo_dominio(x) crea un string y devuelve la parte entre el '@' y el primer '.'
 def solo_dominio(x):
     return x.split('@')[1].split('.')[0].lower()
 
+#creo la columna 'dominio'
 mail_centros['dominio']=''
 
+#a cada mail le asigno su dominio
 for i,e in mail_centros.iterrows():
     mail_centros.loc[i,'dominio']=solo_dominio(e['Mail'])
 
@@ -259,6 +295,8 @@ ON d.id_depto=e.id_depto
 ORDER BY Provincia ASC, Primarias DESC 
                    """).df()
  
+consulta_1.to_csv()   
+ 
 #%% Creo la consulta_2
                    
 consulta_2=dd.sql("""
@@ -298,6 +336,7 @@ ORDER BY Cant_EE DESC, Cant_CC DESC, Provincia ASC, Departamento ASC
 
 #%% Creo la consulta_4
 
+#a cada centro cultural que tiene mail le asigno su dominio
 usa_dominio=dd.sql("""
 SELECT u.ID_cc, m.dominio
 FROM usa_mail AS u
@@ -305,6 +344,7 @@ LEFT JOIN mail_centros AS m
 ON m.Mail=u.Mail            
            """).df()
 
+#a cada departamento le asigno su dominio más usado
 depto_dominio=dd.sql("""
 SELECT c.ID_DEPTO, MAX(u.dominio) AS dominio
 FROM centro_cult AS c
@@ -313,6 +353,7 @@ ON c.ID_cc=u.ID_cc
 GROUP BY ID_DEPTO           
            """).df()
 
+#a cada departamento le asigno su provincia
 consulta_4=dd.sql("""
 SELECT d.prov AS Provincia, d.depto AS Departamento, dd.dominio AS "Dominio más frecuente en CC"
 FROM departamento AS d
@@ -328,7 +369,86 @@ EN ESTA SECCIÓN ESTAREMOS HACIENDO EL PUNTO DE VISUALIZACION
 DE LA SECCIÓN DE ANÁLISIS DE DATOS
 """
 
-#%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import ticker
+import numpy as np
 
+#%% i)
 
+#cuántos cc hay por provincia
+cc_por_prov=dd.sql("""
+SELECT Provincia, SUM(Cant_CC) AS cant_cc
+FROM consulta_3
+GROUP BY Provincia
+ORDER BY cant_cc DESC                 
+                   """).df()
 
+fig,ax=plt.subplots()
+plt.rcParams['font.family']='sans-serif'
+ax.bar(data=cc_por_prov,x='Provincia',height='cant_cc')
+ax.set_title('Cantidad de centros culturales por provincia')
+ax.set_xlabel('')
+ax.set_ylabel('Centros culturales',fontsize='medium')
+plt.xticks(rotation=90)
+ax.set_yticks([])
+ax.bar_label(ax.containers[0],fontsize=8)
+plt.show()
+
+#%% ii)
+
+#tendencia(x,y,c) toma dos columnas de un df (x,y) y un color (c)
+#y devuelve un gráfico de una línea
+def tendencia(x,y,c):
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    plt.plot(x,p(x),color=c,linestyle='--')
+
+fig,ax=plt.subplots()
+plt.rcParams['font.family']='sans-serif'
+ax.scatter(data=consulta_1, x="Poblacion Jardin", y='Jardines',
+           color='red', label='Jardines', s=5)
+tendencia(consulta_1["Poblacion Jardin"],consulta_1['Jardines'],'red')
+ax.scatter(data=consulta_1, x="Poblacion Primaria", y='Primarias',
+           color='skyblue', label='Primarias', s=5)
+tendencia(consulta_1["Poblacion Primaria"],consulta_1['Primarias'], 'skyblue')
+ax.scatter(data=consulta_1, x="Poblacion Secundaria",
+           y='Secundarias', color='orange', label='Secundarias', s=5)
+tendencia(consulta_1["Poblacion Secundaria"],consulta_1['Secundarias'], 'orange')
+plt.legend()
+ax.set_xlabel('Poblacion',fontsize='medium')
+ax.set_ylabel('Establecimientos educativos', fontsize='medium')
+ax.set_title('Cantidad de establecimientos educativos por departamento\n en función de su población')
+ax.set_xlim(0,100000)
+plt.show()
+
+#%% iii)
+
+fig,ax=plt.subplots()
+consulta_3.boxplot(by=['Provincia'], column=['Cant_EE'], ax=ax, grid=False, showmeans=True)
+fig.suptitle('')
+ax.set_title('Cantidad de establecimientos educativos\n de cada departamento por provincia')
+ax.set_xlabel('')
+ax.set_ylabel('Establecimientos educativos')
+plt.xticks(rotation=90)
+ax.set_ylim(0,450)
+plt.show()
+
+#%% iv)
+
+#calculo para cada depto la cantidad de cc cada mil habitantes
+#y la cantidad de ee cada mil habitantes
+cc_ee_1000=dd.sql("""
+SELECT Departamento, ((1000*"Cant_EE")/"Población total") AS ee,
+        ((1000*"Cant_CC")/"Población total") AS cc
+FROM consulta_3
+WHERE Cant_CC>0
+                  """).df()
+
+fig,ax=plt.subplots()
+plt.rcParams['font.family']='sans-serif'
+ax.scatter(data=cc_ee_1000, x='ee',y='cc',s=5)
+ax.set_xlabel('Establecimientos educativos cada mil habitantes', fontsize='medium')
+ax.set_ylabel('Centros culturales cada mil habitantes', fontsize='medium')
+ax.set_title('Cantidad de centros culturales cada mil habitantes\n en función de establecimientos educativos cada mil habitantes')
+plt.show()
